1131cfd4b4fac85874d90fd512744ed767368ff8
iliya <limilia248@gmail.com>
2024-09-12 21:23:08 +0500

    init

---
+++ cmd/bmp/run.go
@@ -0,0 +1,39 @@
+package bmp
+
+import (
+	"fmt"
+	"os"
+
+	"bitmap/logic"
+)
+
+func Run() {
+	if len(os.Args) <= 2 {
+		fmt.Println("Help flag")
+		return
+	}
+	command := os.Args[1]
+	args := os.Args[2:]
+
+	switch command {
+	case "header":
+		if len(args) == 0 {
+			// no source file provided
+			fmt.Printf("Usage: bitmap header <source_file>\nDescription: Prints bitmap file header information\n")
+			os.Exit(1)
+		}
+		logic.Header(args[0])
+	// in dev
+	case "apply":
+		if len(args) < 4 {
+			fmt.Printf("Usage: bitmap apply [--rotate-right|--rotate-left] <input_file> <output_file>\nDescription: Applies transformations to the bitmap file\n")
+			os.Exit(1)
+		}
+		logic.Apply(args[1:])
+	// in dev
+	default:
+		fmt.Println("Error: Unknown command")
+		fmt.Println("Usage: bitmap <command> [options]")
+		os.Exit(1)
+	}
+}
---
+++ internal/parser/parser.go
@@ -0,0 +1,63 @@
+package parser
+
+import (
+	"strings"
+)
+
+const (
+	filter string = "--filter"
+	mirror        = "--mirror"
+	rotate        = "--rotate"
+	crop          = "--crop"
+)
+
+// parser parses args of the apply command
+type Parser struct {
+	rotate    int // 0 means do not rotate {1:90, 2:180, 3:270}
+	horMirror bool
+	verMirror bool
+	filter    string
+	source    string
+	dest      string
+}
+
+// parser() parses args. Returns n parsed args and an error
+func (parser *Parser) parse(args *[]string) (int, error) {
+	for i, arg := range *args {
+		if strings.HasPrefix(arg, filter) {
+			arg = strings.TrimPrefix(arg, filter)
+			parser.filter = arg
+		} else if strings.HasPrefix(arg, rotate) {
+			arg = strings.TrimPrefix(arg, rotate)
+			switch arg {
+			case "right", "90", "-270":
+				parser.rotate = (parser.rotate + 1) % 4
+			case "left", "-90", "270":
+				parser.rotate = (parser.rotate + 3) % 4
+			case "180", "-180":
+				parser.rotate = (parser.rotate + 2) % 4
+			}
+		} else if strings.HasPrefix(arg, mirror) {
+			arg = strings.TrimPrefix(arg, mirror)
+			switch arg {
+			case "horizontal", "h", "horizontally", "hor":
+				parser.horMirror = !parser.horMirror
+			case "vertical", "v", "vertically", "ver":
+				parser.verMirror = !parser.verMirror
+			}
+		} else if strings.HasPrefix(arg, crop) {
+			// in dev
+			arg = strings.TrimPrefix(arg, crop)
+		} else {
+			if i == len(*args)-2 {
+				parser.source = arg
+			} else if i == len(*args)-1 {
+				parser.dest = arg
+			} else {
+				return i, nil // need to return an error not nil
+			}
+		}
+	}
+
+	return len(*args), nil
+}
