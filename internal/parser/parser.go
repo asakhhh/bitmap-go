1beaa80945d5826541afb53bf812baf63c3158b8
iliya <limilia248@gmail.com>
2024-09-13 11:22:49 +0500

    crop-parse

--- internal/parser/parser.go
+++ internal/parser/parser.go
@@ -1,32 +1,35 @@
 package parser
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 const (
-	filter string = "--filter"
-	mirror        = "--mirror"
-	rotate        = "--rotate"
-	crop          = "--crop"
+	filter string = "--filter="
+	mirror        = "--mirror="
+	rotate        = "--rotate="
+	crop          = "--crop="
 )
 
 // parser parses args of the apply command
 type Parser struct {
-	rotate    int // 0 means do not rotate map[int]degrees{0:0, 1:90, 2:180, 3:270}
-	horMirror bool
-	verMirror bool
-	filter    string
-	source    string
-	dest      string
+	rotate     int // 0 means do not rotate map[int]degrees{0:0, 1:90, 2:180, 3:270}
+	horMirror  bool
+	verMirror  bool
+	cropValues []int
+	filter     []string
+	source     string
+	dest       string
 }
 
 // parser() parses args. Returns n parsed args and an error
-func (parser *Parser) parse(args *[]string) (int, error) {
+func (parser *Parser) Parse(args *[]string) (int, error) {
 	for i, arg := range *args {
 		if strings.HasPrefix(arg, filter) {
 			arg = strings.TrimPrefix(arg, filter)
-			parser.filter = arg
+			parser.filter = append(parser.filter, arg)
 		} else if strings.HasPrefix(arg, rotate) {
 			arg = strings.TrimPrefix(arg, rotate)
 			switch arg {
@@ -50,8 +53,43 @@ func (parser *Parser) parse(args *[]string) (int, error) {
 				return i, nil // need to return an error not nil
 			}
 		} else if strings.HasPrefix(arg, crop) {
-			// in dev
 			arg = strings.TrimPrefix(arg, crop)
+			values := strings.Split(arg, "-")
+			numValues := make([]int, 0)
+			fmt.Println(values)
+
+			if len(values) != 2 && len(values) != 4 {
+				// return an error if crop settings are not set properly.
+				// it accepts either two or four values
+				return i, nil // need to return an error not nil
+			}
+
+			for _, str := range values {
+				num, err := strconv.Atoi(str)
+				if err != nil {
+					return i, nil // need to return an error not nil
+				}
+
+				numValues = append(numValues, num)
+			}
+
+			if parser.cropValues == nil {
+				parser.cropValues = numValues
+			} else {
+				if len(numValues) == 2 {
+					parser.cropValues[0] += numValues[0]
+					parser.cropValues[1] += numValues[1]
+				} else {
+					if len(parser.cropValues) == 2 {
+						parser.cropValues = append(parser.cropValues, numValues[2:4]...)
+					} else {
+						parser.cropValues[0] += numValues[0]
+						parser.cropValues[1] += numValues[1]
+						parser.cropValues[2] = numValues[2]
+						parser.cropValues[3] = numValues[3]
+					}
+				}
+			}
 		} else {
 			if i == len(*args)-2 {
 				parser.source = arg
